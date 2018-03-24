ccd8340c6f0aefcc4010cc50db52f958cf30644f
IamI <xinguangyao@gmail.com>
2018-03-24 15:50:00 +0800

    cron

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"./ygopro-analytics"
+	"github.com/jasonlvhit/gocron"
 )
 
 func main() {
 	ygopro_analytics.Initialize()
 	ygopro_analytics.StartServer()
+	gocron.Every(5).Minutes().Do(ygopro_analytics.Push)
 }
